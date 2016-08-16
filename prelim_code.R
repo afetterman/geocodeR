fe403575cda7077dd292c8586bee55c63603b207
makegoodrepeat <adam@makegoodrepeat.com>
2016-08-15 21:22:40 -0700

    third commit

--- prelim_code.R
+++ prelim_code.R
@@ -1,3 +1,11 @@
 # prelim_code.R
 # Geocode addresses using the Google Places API
 
+install.packages("mapproj")
+install.packages("ggmap")
+install.packages("DeducerSpatial")
+
+require(maps)
+require(ggmap)
+
+
